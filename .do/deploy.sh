626c3a1b0bda928d6dafc1b8df036e0d2fb24809
princvaliant <avolos@gmail.com>
2017-05-26 15:28:51 -0700

    - Added deployment scripts for do

---
+++ .do/deploy.sh
@@ -0,0 +1,2 @@
+meteor mup deploy --config=kaiamApp1.js --settings=settings.json
+meteor mup deploy --config=kaiamApp2.js --settings=settings.json
---
+++ .do/setup.sh
@@ -0,0 +1,2 @@
+meteor mup setup --config=kaiamApp1.js --settings=settings.json
+meteor mup setup --config=kaiamApp2.js --settings=settings.json
